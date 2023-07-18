1de2814376311b4fbc99aac227e09a832f3f566d
antwanola <antwanola29@gmail.com>
2023-07-18 06:00:20 +0000

    Hello neighbours

---
+++ Hello.c
@@ -0,0 +1,15 @@
+#include  <stdio.h>
+
+/**
+ * main -entry to the program
+ *
+ * Description: we out here trying to code
+ *
+ * Return: 0 as usual
+ */
+
+int main(void)
+{
+	printf("Howdy neighbor!\nThis is my first c program.\n");
+	return (0);
+}
